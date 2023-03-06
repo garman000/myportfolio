17972ea0b7bfaa12608447de01a1d3bfda6bb027
garman000 <garman000@gmail.com>
2023-03-06 11:11:40 +0100

    added next.config

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "474e567428cff0e8"; }
+/******/ 		__webpack_require__.h = function() { return "ae0b632b3c5c67ff"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.b5fbb7b9481acad6.hot-update.js
+++ .next/static/webpack/webpack.b5fbb7b9481acad6.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "474e567428cff0e8"; }
+/******/ 	__webpack_require__.h = function() { return "ae0b632b3c5c67ff"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
