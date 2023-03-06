3f6921938f6dc28d4aaea20268ee37f46c9c4bae
garman000 <garman000@gmail.com>
2023-03-06 11:59:49 +0100

    removed documentation

--- out/static/webpack/webpack.0aa46542b4256b18.hot-update.js
+++ out/static/webpack/webpack.0aa46542b4256b18.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "3bfddf8044959c47"; }
+/******/ 	__webpack_require__.h = function() { return "b5a0f2f15c46a864"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
